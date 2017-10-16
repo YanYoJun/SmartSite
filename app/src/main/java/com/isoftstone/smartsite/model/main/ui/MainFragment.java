be23bfcf3526df5e81789ac8287f1a0a13c08f3e
yanyongjun <yanyojun@foxmail.com>
2017-10-16 19:25:27 +0800

    增加三方协同主界面

--- app/src/main/java/com/isoftstone/smartsite/model/main/ui/MainFragment.java
+++ app/src/main/java/com/isoftstone/smartsite/model/main/ui/MainFragment.java
@@ -1,20 +1,15 @@
 package com.isoftstone.smartsite.model.main.ui;
 
-import android.content.Context;
-import android.location.Location;
-import android.location.LocationListener;
-import android.location.LocationManager;
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.constraint.solver.ArrayLinkedVariables;
-import android.widget.ImageView;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.isoftstone.smartsite.R;
 import com.isoftstone.smartsite.base.BaseFragment;
 import com.isoftstone.smartsite.http.HomeBean;
 import com.isoftstone.smartsite.http.HttpPost;
-
-import java.util.ArrayList;
 
 
 /**
@@ -38,6 +33,7 @@ public class MainFragment extends BaseFragment{
     private TextView mTemperatureTextView = null;
     private HttpPost mHttpPost = null;
     private HomeBean mHomeBean = null;
+    private Button mThirdPartReport = null; //三方协同按钮
     @Override
     protected int getLayoutRes() {
         return R.layout.fragment_main;
@@ -52,6 +48,14 @@ public class MainFragment extends BaseFragment{
     private void initView(){
         mCityTestView = (TextView) rootView.findViewById(R.id.text_city);
         mTemperatureTextView = (TextView)  rootView.findViewById(R.id.text_temperature);
+        mThirdPartReport = (Button)rootView.findViewById(R.id.button_3);
+        mThirdPartReport.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(),ReportActivity.class);
+                getActivity().startActivity(intent);
+            }
+        });
     }
 
     private void getHomeData(){
---
+++ app/src/main/java/com/isoftstone/smartsite/model/main/ui/ReportActivity.java
@@ -0,0 +1,23 @@
+package com.isoftstone.smartsite.model.main.ui;
+
+import android.os.Bundle;
+
+import com.isoftstone.smartsite.R;
+import com.isoftstone.smartsite.base.BaseActivity;
+
+/**
+ * Created by yanyongjun on 2017/10/16.
+ * 三方协同的主页面
+ */
+
+public class ReportActivity extends BaseActivity {
+    @Override
+    protected int getLayoutRes() {
+        return R.layout.activity_report;
+    }
+
+    @Override
+    protected void afterCreated(Bundle savedInstanceState) {
+
+    }
+}

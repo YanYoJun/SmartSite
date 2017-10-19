1c9ef1d938561a793ad056634b5fda4f01c263c1
TokyoAndroid <vicwudi@126.com>
2017-10-20 00:47:09 +0800

    zhangwei : 地图页增加选项1。

--- app/src/main/java/com/isoftstone/smartsite/common/App.java
+++ app/src/main/java/com/isoftstone/smartsite/common/App.java
@@ -2,13 +2,14 @@ package com.isoftstone.smartsite.common;
 
 import android.app.Application;
 import android.content.Context;
+import android.support.multidex.MultiDexApplication;
 
 
 /**
  * Created by zw on 2017/10/11.
  */
 
-public class App extends Application {
+public class App extends MultiDexApplication {
 
     private static App mInstance;
 

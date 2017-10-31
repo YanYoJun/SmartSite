4532a196339c110f7f088d59a77279faf2c7c6b6
yanyongjun <yanyojun@foxmail.com>
2017-10-31 22:37:38 +0800

    三方协同界面优化

--- app/src/main/java/com/isoftstone/smartsite/model/tripartite/fragment/RevisitFragment.java
+++ app/src/main/java/com/isoftstone/smartsite/model/tripartite/fragment/RevisitFragment.java
@@ -1,5 +1,6 @@
 package com.isoftstone.smartsite.model.tripartite.fragment;
 
+import android.app.DatePickerDialog;
 import android.content.res.Resources;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
@@ -7,6 +8,7 @@ import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.GridView;
 import android.widget.SimpleAdapter;
@@ -14,8 +16,10 @@ import android.widget.TextView;
 
 import com.isoftstone.smartsite.R;
 import com.isoftstone.smartsite.base.BaseFragment;
+import com.isoftstone.smartsite.model.tripartite.data.ITime;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -38,10 +42,21 @@ public class RevisitFragment extends BaseFragment {
     private TextView mName = null;
     private TextView mReportName = null;
     private TextView mReportMsg = null;
+    private TextView mBeginTime = null;
+    private TextView mEndTime = null;
+    private TextView mRevisitTime = null;
+    private TextView mLabTime = null;
 
     private EditText mEditName = null;
     private EditText mEditReportName = null;
     private EditText mEditReportMsg = null;
+    private TextView mEditRevisitTime = null;
+
+    private ITime mBeginDate = null;
+    private ITime mEndDate = null;
+    private ITime mRevisitDate = null;
+
+    private Calendar mCal = null;
 
 
     @Override
@@ -61,6 +76,7 @@ public class RevisitFragment extends BaseFragment {
         mWattingChanged = mRes.getDrawable(R.drawable.editcolumn);
         mWattingChanged.setBounds(0, 0, mWattingChanged.getIntrinsicWidth(), mWattingChanged.getIntrinsicHeight());
 
+        mCal = Calendar.getInstance();
         initView();
         initListener();
         initGridView();
@@ -70,13 +86,75 @@ public class RevisitFragment extends BaseFragment {
         mName = (TextView) getView().findViewById(R.id.lab_name);
         mReportName = (TextView) getView().findViewById(R.id.lab_report_name);
         mReportMsg = (TextView) getView().findViewById(R.id.lab_report_msg);
+        mBeginTime = (TextView) getView().findViewById(R.id.lab_begin_time);
+        mEndTime = (TextView) getView().findViewById(R.id.lab_end_time);
+        mRevisitTime = (TextView) getView().findViewById(R.id.lab_revisit_time);
+        mLabTime = (TextView) getView().findViewById(R.id.lab_inspect_report_time);
 
         mEditName = (EditText) getView().findViewById(R.id.edit_name);
         mEditReportName = (EditText) getView().findViewById(R.id.edit_report_name);
         mEditReportMsg = (EditText) getView().findViewById(R.id.edit_report_msg);
+        mEditRevisitTime = (TextView) getView().findViewById(R.id.lab_edit_revisit_time);
     }
 
     private void initListener() {
+        mBeginTime.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+/*                final Calendar c = Calendar.getInstance();*/
+                DatePickerDialog dialog = new DatePickerDialog(getActivity(), new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                        mBeginTime.setText("" + year + "-" + (monthOfYear + 1) + "-" + dayOfMonth);
+                        mBeginDate = new ITime(year, monthOfYear + 1, dayOfMonth);
+                        mBeginTime.setTextColor(mRes.getColor(R.color.main_text_color));
+                        if (mBeginDate != null && mEndDate != null) {
+                            mLabTime.setCompoundDrawables(mWattingChanged, null, null, null);
+                        }
+                    }
+                }, mCal.get(Calendar.YEAR), mCal.get(Calendar.MONTH), mCal.get(Calendar.DAY_OF_MONTH));
+                dialog.show();
+            }
+
+        });
+
+        mEndTime.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+/*                final Calendar c = Calendar.getInstance();*/
+                DatePickerDialog dialog = new DatePickerDialog(getActivity(), new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                        mEndTime.setText("" + year + "-" + (monthOfYear + 1) + "-" + dayOfMonth);
+                        mEndDate = new ITime(year, monthOfYear + 1, dayOfMonth);
+                        mEndTime.setTextColor(mRes.getColor(R.color.main_text_color));
+                        if (mBeginDate != null && mEndDate != null) {
+                            mLabTime.setCompoundDrawables(mWattingChanged, null, null, null);
+                        }
+                    }
+                }, mCal.get(Calendar.YEAR), mCal.get(Calendar.MONTH), mCal.get(Calendar.DAY_OF_MONTH));
+                dialog.show();
+            }
+
+        });
+
+        mEditRevisitTime.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+/*                final Calendar c = Calendar.getInstance();*/
+                DatePickerDialog dialog = new DatePickerDialog(getActivity(), new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                        mEditRevisitTime.setText("" + year + "-" + (monthOfYear + 1) + "-" + dayOfMonth);
+                        mEditRevisitTime.setTextColor(mRes.getColor(R.color.main_text_color));
+                        mRevisitDate = new ITime(year, monthOfYear + 1, dayOfMonth);
+                        mRevisitTime.setCompoundDrawables(mWattingChanged, null, null, null);
+                    }
+                }, mCal.get(Calendar.YEAR), mCal.get(Calendar.MONTH), mCal.get(Calendar.DAY_OF_MONTH));
+                dialog.show();
+            }
+
+        });
         mEditName.addTextChangedListener(new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence s, int start, int count, int after) {

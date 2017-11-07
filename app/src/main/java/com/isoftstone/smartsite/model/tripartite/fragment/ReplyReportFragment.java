d3b469becb18587a6fd084383a3fc49a06633bbc
yanyongjun <yanyojun@foxmail.com>
2017-11-07 23:19:00 +0800

    回复界面添加附件处理

--- app/src/main/java/com/isoftstone/smartsite/model/tripartite/fragment/ReplyReportFragment.java
+++ app/src/main/java/com/isoftstone/smartsite/model/tripartite/fragment/ReplyReportFragment.java
@@ -1,5 +1,8 @@
 package com.isoftstone.smartsite.model.tripartite.fragment;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.text.TextUtils;
@@ -9,20 +12,21 @@ import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.GridView;
-import android.widget.SimpleAdapter;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.isoftstone.smartsite.R;
 import com.isoftstone.smartsite.base.BaseActivity;
 import com.isoftstone.smartsite.base.BaseFragment;
+import com.isoftstone.smartsite.http.PatrolBean;
 import com.isoftstone.smartsite.http.ReportBean;
+import com.isoftstone.smartsite.model.tripartite.activity.TripartiteActivity;
+import com.isoftstone.smartsite.model.tripartite.adapter.AttachGridViewAdatper;
 import com.isoftstone.smartsite.utils.DateUtils;
+import com.isoftstone.smartsite.utils.FilesUtils;
 
 import java.util.ArrayList;
 import java.util.Date;
-import java.util.HashMap;
-import java.util.Map;
 
 /**
  * Created by yanyongjun on 2017/10/30.
@@ -30,13 +34,16 @@ import java.util.Map;
 
 public class ReplyReportFragment extends BaseFragment {
     private GridView mAttachView = null;
-    private SimpleAdapter mAttachAdapter = null;
-    private ArrayList<Map<String, Object>> mData = null;
+    private AttachGridViewAdatper mAttachAdapter = null;
+    private ArrayList<Object> mData = null;
 
     private EditText mEditContent = null;
     private Button mSubButton = null;
     private BaseActivity mActivity = null;
     private TextView mLabName = null;
+    public final static int REQUEST_ACTIVITY_ATTACH = 0;//请求图片的request code
+    private ArrayList<String> mFilesPath = new ArrayList<>();
+
 
     @Override
     protected int getLayoutRes() {
@@ -67,29 +74,28 @@ public class ReplyReportFragment extends BaseFragment {
                 }
             }
         });
-        mLabName = (TextView)rootView.findViewById(R.id.lab_report_people_name);
+        mLabName = (TextView) rootView.findViewById(R.id.lab_report_people_name);
         mLabName.setText(mHttpPost.mLoginBean.getmName());
     }
 
     public void initGridView() {
         mAttachView = (GridView) getView().findViewById(R.id.grid_view);
 
-        mData = new ArrayList<Map<String, Object>>();
-        Map<String, Object> data = new HashMap<>();
-        data.put("image", R.drawable.attachment);
-        mData.add(data);
-        mAttachAdapter = new SimpleAdapter(getActivity(), mData, R.layout.add_attach_grid_item, new String[]{"image"}, new int[]{R.id.image});
+        mData = new ArrayList<Object>();
+        mData.add(R.drawable.attachment);
+        //mAttachAdapter = new SimpleAdapter(getActivity(), mData, R.layout.add_attach_grid_item, new String[]{"image"}, new int[]{R.id.image});
+        mAttachAdapter = new AttachGridViewAdatper(getActivity(), mData);
         mAttachView.setAdapter(mAttachAdapter);
 
         mAttachView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-/*                if (position == mData.size() - 1) {
+                if (position == mData.size() - 1) {
                     //点击添加附件
                     Intent i = new Intent(Intent.ACTION_GET_CONTENT);
-                    i.setType("image*//*");
+                    i.setType("*/*");
                     startActivityForResult(i, REQUEST_ACTIVITY_ATTACH);
-                }*/
+                }
             }
         });
     }
@@ -102,8 +108,10 @@ public class ReplyReportFragment extends BaseFragment {
     }
 
     private ReportBean getReportBean() {
+        PatrolBean tempBean = new PatrolBean();
+        tempBean.setId(mActivity.getReportData().getId());
         ReportBean reportBean = new ReportBean();
-        reportBean.setPatrol(mActivity.getReportData());
+        reportBean.setPatrol(tempBean);
         //reportBean.setCreator(mHttpPost.mLoginBean.getmName());
         reportBean.setContent(mEditContent.getText().toString()); //TODO
         reportBean.setDate(DateUtils.format2.format(new Date()));
@@ -112,7 +120,7 @@ public class ReplyReportFragment extends BaseFragment {
         return reportBean;
     }
 
-    private class DealTask extends AsyncTask<Void, Void, Void> {
+    private class DealTask extends AsyncTask<Void, Void, Boolean> {
         private ReportBean mBean = null;
 
         public DealTask(ReportBean reportBean) {
@@ -120,16 +128,87 @@ public class ReplyReportFragment extends BaseFragment {
         }
 
         @Override
-        protected Void doInBackground(Void... voids) {
+        protected Boolean doInBackground(Void... voids) {
             Log.e(TAG, "deal task begin");
-            mHttpPost.addPatrolReply(mBean);
-            return null;
+            try {
+                mHttpPost.addPatrolReply(mBean);
+                if (mFilesPath != null && mFilesPath.size() >= 1) {
+                    PatrolBean report = mHttpPost.getPatrolReport(mBean.getPatrol().getId() + "");
+                    ArrayList<ReportBean> reports = report.getReports();
+                    int id = report.getReports().get(reports.size() - 1).getId();
+                    for (String path : mFilesPath) {
+                        mHttpPost.reportFileUpload(path, id);
+                    }
+                }
+                return true;
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return false;
         }
 
         @Override
-        protected void onPostExecute(Void aVoid) {
-            super.onPostExecute(aVoid);
-            getActivity().finish();
+        protected void onPostExecute(Boolean temp) {
+            super.onPostExecute(temp);
+            if (temp == true) {
+                Toast.makeText(getActivity(), "添加回复成功", Toast.LENGTH_SHORT).show();
+                getActivity().finish();
+            } else {
+                Toast.makeText(getActivity(), "添加回复失败", Toast.LENGTH_SHORT).show();
+            }
         }
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        switch (requestCode) {
+            case REQUEST_ACTIVITY_ATTACH: {
+                Log.e(TAG, "onActivityResult:" + data);
+                if (resultCode == Activity.RESULT_OK) {
+                    Uri uri = data.getData();
+                    Log.e(TAG, "yanlog uri:" + uri);
+                    if ("file".equalsIgnoreCase(uri.getScheme())) {//使用第三方应用打开
+                        //Toast.makeText(getActivity(), uri.getPath() + "11111", Toast.LENGTH_SHORT).show();
+                        addAttach(uri.getPath(), uri.toString());
+                        return;
+                    }
+                    String path = FilesUtils.getPath(getActivity(), uri);
+                    //Toast.makeText(getActivity(), path, Toast.LENGTH_SHORT).show();
+                    addAttach(path, uri.toString());
+                }
+            }
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    //add files
+    public void addAttach(String path, String uri) {
+        Log.e(TAG, "yanlog remove begin size:" + mData.size());
+        String formatPath = FilesUtils.getFormatString(path);
+        Log.e(TAG, "yanlog remove begin size at0:" + mData.get(0));
+        mData.remove(mData.size() - 1);
+        mFilesPath.add(path);
+        if (TripartiteActivity.mImageList.contains(formatPath)) {
+            mData.add(uri);
+        } else if (TripartiteActivity.mXlsList.contains(formatPath)) {
+            mData.add(TripartiteActivity.mAttach.get(".xls"));
+        } else if (TripartiteActivity.mDocList.contains(formatPath)) {
+            mData.add(TripartiteActivity.mAttach.get(".doc"));
+        } else if (TripartiteActivity.mPdfList.contains(formatPath)) {
+            mData.add(TripartiteActivity.mAttach.get(".pdf"));
+        } else if (TripartiteActivity.mPptList.contains(formatPath)) {
+            mData.add(TripartiteActivity.mAttach.get(".ppt"));
+        } else if (TripartiteActivity.mVideoList.contains(formatPath)) {
+            mData.add(TripartiteActivity.mAttach.get(".video"));
+        } else {
+            mData.add(TripartiteActivity.mAttach.get(".doc"));
+        }
+
+        mData.add(R.drawable.attachment);
+        Log.e(TAG, "yanlog remove end size:" + mData.size());
+        Log.e(TAG, "yanlog mData at 0:" + mData.get(0));
+        mAttachAdapter.notifyDataSetChanged();
+        mAttachView.requestLayout();
+        mAttachView.setMinimumHeight(600);
     }
 }

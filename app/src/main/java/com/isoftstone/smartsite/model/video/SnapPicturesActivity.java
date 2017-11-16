6d2640f01074915ff1bf557e309a2e7b9fd497e6
zhangyinfu <zhangyinfu@outlook.com>
2017-11-17 02:05:51 +0800

    1、修复N版本无法打开图片显示问题

--- app/src/main/java/com/isoftstone/smartsite/model/video/SnapPicturesActivity.java
+++ app/src/main/java/com/isoftstone/smartsite/model/video/SnapPicturesActivity.java
@@ -4,7 +4,10 @@ import android.Manifest;
 import android.app.Activity;
 import android.content.Intent;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.v4.content.FileProvider;
+import android.util.Log;
 import android.view.View;
 import android.widget.AbsListView;
 import android.widget.AbsListView.OnScrollListener;
@@ -20,6 +23,8 @@ import com.isoftstone.smartsite.model.video.bean.PhotoList;
 import com.isoftstone.smartsite.model.video.utils.CheckImageLoaderConfiguration;
 import com.isoftstone.smartsite.model.video.utils.UniversalImageLoadTool;
 import com.isoftstone.smartsite.utils.ToastUtils;
+
+import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -57,10 +62,22 @@ public class SnapPicturesActivity extends Activity {
 			public void onItemClick(AdapterView<?> parent, View view,
 									int position, long id) {
 				//打开指定的一张照片
-				String pictureFilepath = list.get(position).getPath_file();
+				String pictureFilepath = list.get(position).getPath_absolute();
 				Intent intent = new Intent();
 				intent.setAction(android.content.Intent.ACTION_VIEW);
-				intent.setDataAndType(Uri.parse(pictureFilepath), "image/*");
+				//ToastUtils.showShort(pictureFilepath);
+				Log.i("zyf", "zzz   : " + pictureFilepath );
+				File file = new File(pictureFilepath);
+				Uri imageUri ;
+				if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+					//申请权限
+					intent.setFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+					//getUriForFile的第二个参数就是Manifest中的authorities
+					imageUri = FileProvider.getUriForFile(getApplicationContext(), "com.isoftstone.smartsite.fileProvider", file);
+				} else {
+					imageUri = Uri.fromFile(file);//Uri.parse(pictureFilepath)
+				}
+				intent.setDataAndType(imageUri, "image/*");
 				startActivity(intent);
 
 				//打开系统相册浏览照片  
